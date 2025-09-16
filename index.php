611a8d6fe61e731d623645dcd4cc5e0a29545b09
auliaassyifasari <auliaassyifasari@gmail.com>
2025-09-16 13:18:15 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client();
+$response = $client->request('GET', 'https://jsonplaceholder.typicode.com/posts/1');
+
+echo $response->getBody()
